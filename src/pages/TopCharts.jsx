1873207db4cf63db03e78f5ad912d952f8ff12a2
kepha222 <kephamotari1@.com>
2022-11-20 16:19:14 +0300

    third commit

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,41 +1,80 @@
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
-import { Error, Loader, SongCard } from "../components";
-//import { genres } from "../assets/constants";
+import { Error, Loader } from "../components";
 import { useGetTopChartsHistoryQuery } from "../redux/services/shazamCore";
 
 const TopCharts = () => {
-  // const dipatch = useDispatch();
+  const [dailyData, setDailyData] = useState([]);
 
   const { data, isFetching, error } = useGetTopChartsHistoryQuery();
 
   if (isFetching) return <Loader title="Loading..." />;
+
+  console.log(data?.response);
+
+  const dataHistory = data?.response;
+
   if (error) return <Error />;
 
-  const dataHistoy = data?.response?.cases;
+  // useEffect(() => {
+  //   const fetchAPI = async () => {
+  //     setDailyData(await useGetTopChartsHistoryQuery());
+  //   };
 
-  //console.log(dataHistoy);
+  //   //console.log(dailyData)
 
+  //   fetchAPI();
+  // }, []);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      dataHistory={{
+        labels: dailyData(({ date }) => date),
+        datasets: [
+          {
+            dataHistory: dailyData.map(({ cases }) => cases),
+            label: "Cases",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            dataHistory: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            background: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+          {
+            dataHistory: dailyData.map(({ tests }) => tests),
+            label: "Tests",
+            borderColor: "green",
+            background: "#0071bd",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
   return (
-    <div className="flex flex-col">
-      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">
-          Covid19 Analysis System
-        </h2>
+    <>
+      <div className="flex flex-col">
+        <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+          <h2 className="font-bold text-3xl text-white text-left">
+            Covid19 Analysis System
+          </h2>
+        </div>
+
+        <div className="">{lineChart}</div>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {dataHistoy?.map((song, i) => (
-          <SongCard
-            key={song.key}
-            dataHistoy={dataHistoy}
-            activeSong={activeSong}
-            song={song}
-            i={i}
-          />
-        ))}
+      <div className="flex flex-wrap sm:justify-start justify-center">
+        {dataHistory?.map((dailyData) => ({
+          cases: dailyData.cases.total,
+          deaths: dailyData.deaths.total,
+          tests: dailyData.tests.total,
+        }))}
       </div>
-    </div>
+    </>
   );
 };
 
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -33,21 +33,12 @@ export const shazamCoreApi = createApi({
       query: (searchTerm) => `/statistics?country=${searchTerm}`,
     }),
 
-    getTopChartsHistory: builder.query({ query: () => "/history" }),
-
-    // getSongsByGenre: builder.query({query:(genre) => `/charts/genre-world?genre_code=${genre}`},)
-    //getSongDetails: builder.query({ query: ({songid}) => `/tracks/details?track_id=${songid}` }),
-    //getSongRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
-    //getArtistDetails: builder.query({ query: ({artistId}) => `/artists/details?artist_id=${artistid}` }),
-    // getSongsByCountry: builder.query({query: (countryCode) => `/charts/country?country_code=${countryCode}`} ),
+    getTopChartsHistory: builder.query({
+      query: (time) => `/history?day=${time}`,
+    }),
   }),
 });
 
-//useGetSongDetailsQuery
-//useGetSongsByGenreQuery
-//useGetSongRelatedQuery
-//useGetArtistDetailsQuery
-//useGetSongsByCountryQuery
 export const {
   useGetTopChartsQuery,
   useGetSongsBySearchQuery,
